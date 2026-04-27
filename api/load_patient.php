4f59c436689ea96cbbaf2da4563868b315b890a9
ad-edu <arijit.dey.edu@outlook.com>
2026-04-27 11:27:11 +0530

    Committing latest changes

--- api/load_patient.php
+++ api/load_patient.php
@@ -16,7 +16,7 @@ if (!file_exists($file)) {
         'uhid' => '722604000958', 'hospitalSys' => 120, 'hospitalDia' => 80,
         'visitDate' => '2026-04-17', 'caseType' => 'New Case',
         'device' => 'Omron BP Machine Fully Automatic with Heart Rate',
-        'instruction' => 'Check on right hand only',
+        'instruction' => '',
         'morningReminderOn' => false, 'morningReminderTime' => '08:45',
         'eveningReminderOn' => false, 'eveningReminderTime' => '20:45',
         'exists' => false,
